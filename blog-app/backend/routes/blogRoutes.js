0a9c223181d82ad3ccd0cc43bbb72a2089f319a4
Vibinchandar <vibinmarutharajavp@gmail.com>
2025-09-28 23:36:53 +0530

    Implemented JWT Authentication Middleware

---
+++ blog-app/backend/routes/blogRoutes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const Blog = require("../models/Blog");
+const { protect } = require("../middleware/authMiddleware");
+
+// Get all blogs
+router.get("/", async (req, res) => {
+  const blogs = await Blog.find().populate("author", "name").sort({ createdAt: -1 });
+  res.json(blogs);
+});
+
+// Get single blog
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("author", "name");
+  if (blog) res.json(blog);
+  else res.status(404).json({ message: "Blog not found" });
+});
+
+// Create blog
+router.post("/", protect, async (req, res) => {
+  const { title, content } = req.body;
+  const blog = new Blog({ title, content, author: req.user._id });
+  await blog.save();
+  res.status(201).json(blog);
+});
+
+// Update blog
+router.put("/:id", protect, async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) return res.status(404).json({ message: "Blog not found" });
+  if (blog.author.toString() !== req.user._id.toString()) return res.status(401).json({ message: "Not authorized" });
+
+  blog.title = req.body.title || blog.title;
+  blog.content = req.body.content || blog.content;
+  await blog.save();
+  res.json(blog);
+});
+
+// Delete blog
+router.delete("/:id", protect, async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) return res.status(404).json({ message: "Blog not found" });
+  if (blog.author.toString() !== req.user._id.toString()) return res.status(401).json({ message: "Not authorized" });
+
+  await blog.remove();
+  res.json({ message: "Blog removed" });
+});
+
+module.exports = router;
